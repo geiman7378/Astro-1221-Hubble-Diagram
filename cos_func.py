7c428cd9226fc798fc626c856d28acb7788c0a6a
ajfomby <austinfomby@gmail.com>
2026-04-15 14:46:15 -0400

    new file:   cos_func.py     outline of class cos_func

---
+++ cos_func.py
@@ -0,0 +1,8 @@
+# define class
+# inputs Ho, omega(m), z, omega(k), omega(A)
+# H(z) = Ho * sqrt[omega(m)*(1+z)^3 + omega(k)(1+z)^2 + omega(A)]
+# omega(k) = 1 - (omega(m) + omega(A))
+
+# separate low z (z < 0.5) and high z data (z < 0.5)
+
+# assume Ho of 70 to start with until Joseph gets done with code, then use his derived value
